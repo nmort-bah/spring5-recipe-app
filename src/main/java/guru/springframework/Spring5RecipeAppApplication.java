e5967779e3d7536f6cfdb7c51af6766eb9b369ca
nmort-bah <624953@bah.com>
2022-07-14 11:30:07 -0400

    Complete assignment

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -1,7 +1,8 @@
-package guru.springframework.spring5recipeapp;
+package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class Spring5RecipeAppApplication {
