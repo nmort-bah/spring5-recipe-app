b3b173219112f00e01ae9e232fa83dca5778fb47
nmort-bah <624953@bah.com>
2022-08-02 13:31:19 -0400

    Trying to fix Socket Failure using Docker for mongodb

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -3,8 +3,9 @@ package guru.springframework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication//(exclude = MongoAutoConfiguration.class)
 public class Spring5RecipeAppApplication {
 
 	public static void main(String[] args) {
